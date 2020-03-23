11ac964a1639f12da5bc9aa35f0fc39ad900999c
ftx <aakubrakov@yandex.com>
2020-03-23 20:18:22 +0400

    add spring boot and kotlin spring support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
-plugins {
-    kotlin("jvm") version "1.3.61"
-}
-
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+plugins {
+    id("org.springframework.boot") version "2.2.2.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    kotlin("jvm") version "1.3.61"
+    kotlin("plugin.spring") version "1.3.61"
+}
 
 repositories {
     mavenCentral()
@@ -11,6 +14,11 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    runtimeOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks {
