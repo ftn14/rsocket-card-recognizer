7a47ae62da19663b65e68ee3ccf61b1d8c300868
ftx14 <aakubrakov@yandex.com>
2020-05-21 10:00:43 +0300

    update RSocketClientTest

--- src/test/kotlin/com/ftx/h1/inside/RSocketClientTest.kt
+++ src/test/kotlin/com/ftx/h1/inside/RSocketClientTest.kt
@@ -2,11 +2,18 @@ package com.ftx.h1.inside
 
 import com.ftx.h1.inside.client.RSocketClient
 import org.junit.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.test.context.ActiveProfiles
 
+@Suppress("EXPERIMENTAL_API_USAGE")
+@TestConfiguration
+@ActiveProfiles("test")
+@SpringBootTest
 class RSocketClientTest(private val rSocketClient: RSocketClient) {
 
     @Test
     fun test() {
-
+        rSocketClient.sendHand("TEST_HAND")
     }
 }
